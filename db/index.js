049f3ac1a6ded655f5720158794067936156be25
nimi7 <62261707+nimi7@users.noreply.github.com>
2021-08-27 18:21:18 +0300

    uri

--- db/index.js
+++ db/index.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 mongoose
-    .connect( "mongodb+srv://Nimrod-Admin:nimi0528266913@cluster0.yzitu.mongodb.net/Tilok?retryWrites=true&w=majority",{ useNewUrlParser: true,useUnifiedTopology:true })
+    .connect(process.env.URI || "mongodb+srv://Nimrod-Admin:nimi0528266913@cluster0.yzitu.mongodb.net/Tilok?retryWrites=true&w=majority",{ useNewUrlParser: true,useUnifiedTopology:true })
     .catch(e => {
         console.error('Connection error', e.message)
     });
